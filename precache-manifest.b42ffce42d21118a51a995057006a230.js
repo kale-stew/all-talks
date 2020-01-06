46de88046468ee5ef8c8c89f5c5293f800145e03
Kylie Stewart <kylieastewart@gmail.com>
2020-01-05 20:42:07 -0700

    Updates

---
+++ precache-manifest.b42ffce42d21118a51a995057006a230.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8b42ec7d5fa12319b03952ccb5aff3",
+    "url": "/talks/index.html"
+  },
+  {
+    "revision": "714fdd81255e87abfa83",
+    "url": "/talks/static/css/main.cec8aa05.chunk.css"
+  },
+  {
+    "revision": "87c2a226f47694509b76",
+    "url": "/talks/static/js/2.a1b3e900.chunk.js"
+  },
+  {
+    "revision": "714fdd81255e87abfa83",
+    "url": "/talks/static/js/main.3d7badfc.chunk.js"
+  },
+  {
+    "revision": "485aa6bc8a895181aef1",
+    "url": "/talks/static/js/runtime-main.ece4adfc.js"
+  },
+  {
+    "revision": "602af25feb9d4873fa4342610c21ab0b",
+    "url": "/talks/static/media/email.602af25f.png"
+  },
+  {
+    "revision": "db4fb8da40c75e7c8f5440924d581ed6",
+    "url": "/talks/static/media/github.db4fb8da.png"
+  },
+  {
+    "revision": "699a77c2772e5c07fc7bfd99b3ec616b",
+    "url": "/talks/static/media/instagram.699a77c2.png"
+  },
+  {
+    "revision": "c7019d129c209c7b22f3711060ab99c2",
+    "url": "/talks/static/media/linkedin.c7019d12.png"
+  },
+  {
+    "revision": "614e6458f1fdcb9e172fb2e51b9cc756",
+    "url": "/talks/static/media/twitter.614e6458.png"
+  }
+]);
