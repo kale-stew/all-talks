885a80def7697e13f175a1229253844c3811a43c
Kylie Stewart <kylieastewart@gmail.com>
2020-01-05 20:34:57 -0700

    move talks into separate dir, add a lander

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import Footer from "./components/footer";
+import "./app.css";
+
+export default function App() {
+  return (
+    <Fragment style={{ height: "100%" }}>
+      <body>
+        <header />
+        <div className="content">
+          <h1>Talks!</h1>
+        </div>
+        <Footer />
+      </body>
+    </Fragment>
+  );
+}
---
+++ src/components/footer.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { withStyles } from "@material-ui/core";
+import Avatar from "@material-ui/core/Avatar";
+import "../app.css";
+
+const images = {
+  LogoGithub: require("../logos/github.png"),
+  LogoInstagram: require("../logos/instagram.png"),
+  LogoLinkedIn: require("../logos/linkedin.png"),
+  LogoMail: require("../logos/email.png"),
+  LogoTwitter: require("../logos/twitter.png")
+};
+
+const KsAvatar = withStyles({
+  root: {
+    background: "transparent"
+  }
+})(Avatar);
+
+export default function Footer() {
+  return (
+    <footer>
+      <a href="https://kyliestewart.tech">
+        <KsAvatar>KS</KsAvatar>
+      </a>
+      <a href="https://twitter.com/kyliestew">
+        <Avatar
+          alt="Link to Kylie's Twitter account"
+          src={images.LogoTwitter}
+        />
+      </a>
+      <a href="https://www.linkedin.com/in/kylieastewart/">
+        <Avatar
+          alt="Link to Kylie's LinkedIn account"
+          src={images.LogoLinkedIn}
+        />
+      </a>
+      <a href="https://instagram.com/kalestews">
+        <Avatar
+          alt="Link to Kylie's Instagram account"
+          src={images.LogoInstagram}
+        />
+      </a>
+      <a href="mailto:kylie@kyliestewart.tech">
+        <Avatar alt="Link to email Kylie" src={images.LogoMail} />
+      </a>
+    </footer>
+  );
+}
