8592614ea225fe7e41e1beb8121742966509f996
Kylie Stewart <kylie@kyliestewart.tech>
2020-03-08 21:48:42 -0600

    add a Card component

---
+++ src/components/card.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const CardWrapper = styled('div')`
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+`;
+
+const EventInfo = styled('div')`
+  padding: 2rem 0;
+`;
+
+const ImgWrapper = styled('div')``;
+
+const TalkDescription = styled('span')``;
+
+const TalkTitle = styled('span')``;
+
+const Card = ({ talk }) => (
+  <CardWrapper>
+    <TalkTitle>{talk.title}</TalkTitle>
+    <TalkDescription>{talk.description}</TalkDescription>
+    {talk.previewImg && (
+      <ImgWrapper>
+        <img src={talk.previewImg} alt={`${talk.title} slide preview`} />
+      </ImgWrapper>
+    )}
+    <EventInfo>
+      <span style={{ paddingRight: '1rem' }}>{talk.eventName}</span>
+      <span>{talk.eventDate}</span>
+    </EventInfo>
+    <a
+      href={talk.exportedSlidesUrl}
+      title={`Link to exported slides for ${talk.title}`}
+    >
+      <span>See a PDF of slides</span>
+    </a>
+    <a
+      href={talk.hostedSlidesUrl}
+      title={`Link to exported slides for ${talk.title}`}
+    >
+      <span>See the live slides</span>
+    </a>
+    {talk.recordedPresentationUrl && (
+      <a
+        href={talk.recordedPresentationUrl}
+        title={`Link to exported slides for ${talk.title}`}
+      >
+        <span>See a recording of the presentation</span>
+      </a>
+    )}
+  </CardWrapper>
+);
+
+Card.propTypes = {
+  talk: PropTypes.objectOf({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    eventDate: PropTypes.string.isRequired,
+    eventName: PropTypes.string,
+    eventType: PropTypes.string,
+    exportedSlidesUrl: PropTypes.string.isRequired,
+    hostedSlidesUrl: PropTypes.string.isRequired,
+    previewImg: PropTypes.string,
+    recordedPresentationUrl: PropTypes.string
+  })
+};
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+
+import Footer from './components/footer';
+import * as Talks from './talks.json';
 import './index.css';
-import App from './app';
+import Card from './components/card';
+
+// TODO - regex to pull the year from eventDate
+// const checkYear = (date) => date;
+
+// Talks.map(t => checkYear(t.eventDate)) === '2020' ? t : null){}
+
+const App = () => (
+  <Fragment style={{ height: '100%' }}>
+    <body>
+      <header />
+      <div className="content">
+        <h1>Kylie's Talks</h1>
+        <h4>All of my talks, slides, and presentations, in one place.</h4>
+
+        <h2>2020</h2>
+        <Card talk={Talks[0]} />
+
+        <h2>2019</h2>
+        <div>{}</div>
+
+        <h2>2018</h2>
+        <div>{}</div>
+      </div>
+      <Footer />
+    </body>
+  </Fragment>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
